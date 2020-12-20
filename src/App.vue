9c26a4d7e50fce42f7fe455c9494b25e6355d751
nzpznk <skynnznk12@163.com>
2020-12-21 07:45:57 +0800

    search box ui

--- src/App.vue
+++ src/App.vue
@@ -14,42 +14,6 @@ import Navbar from './Navbar.vue'
 export default {
   components: {
     Navbar
-  },
-  methods: {
-    updatePage(data) {
-      console.log('updating page...');
-      console.log(data);
-      switch (data.keyPath[0]) {
-        case '1':
-          console.log('每日')
-          break;
-        case '2':
-          console.log('每周')
-          break;
-        case '3':
-          console.log('每月')
-          break;
-        case '4':
-          console.log('随机')
-          break;
-        case '5':
-          if (data.keyPath[1] == '5-1') {
-            console.log('已读')
-          } else if (data.keyPath[1] == '5-2') {
-            console.log('已赞')
-          } else if (data.keyPath[1] == '5-3') {
-            console.log('分享')
-          } else {
-            console.log('未知标签')
-          }
-          break;
-      }
-    }
-  }, 
-  data() {
-    return {
-      checked: true
-    };
   }
 }
 </script>
--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,24 +1,41 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b">
-    <el-menu-item index="1"><router-link to="/daily">每日热门文章</router-link></el-menu-item>
-    <el-menu-item index="2"><router-link to="/weekly">每周热门文章</router-link></el-menu-item>
-    <el-menu-item index="3"><router-link to="/monthly">每月热门文章</router-link></el-menu-item>
-    <el-menu-item index="4"><router-link to="/randomly">随机推荐</router-link></el-menu-item>
-    <el-submenu index="5">
-      <template slot="title">我的</template>
-      <el-menu-item index="5-1"><router-link to="readed">已读</router-link></el-menu-item>
-      <el-menu-item index="5-2"><router-link to="agreed">赞过</router-link></el-menu-item>
-      <el-menu-item index="5-3"><router-link to="shared">分享</router-link></el-menu-item>
-    </el-submenu>
-    <div class="block"><el-avatar shape="square" :size="60" src="./assets/avatar.png"></el-avatar></div>
-  </el-menu>
+  <el-row class='navbar' type="flex" align="middle">
+    <el-col :span="12">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1"><router-link to="/daily">每日热门文章</router-link></el-menu-item>
+        <el-menu-item index="2"><router-link to="/weekly">每周热门文章</router-link></el-menu-item>
+        <el-menu-item index="3"><router-link to="/monthly">每月热门文章</router-link></el-menu-item>
+        <el-menu-item index="4"><router-link to="/randomly">随机推荐</router-link></el-menu-item>
+        <!-- <el-submenu index="5">
+          <template slot="title">我的</template>
+          <el-menu-item index="5-1"><router-link to="readed">已读</router-link></el-menu-item>
+          <el-menu-item index="5-2"><router-link to="agreed">赞过</router-link></el-menu-item>
+          <el-menu-item index="5-3"><router-link to="shared">分享</router-link></el-menu-item>
+        </el-submenu> -->
+      </el-menu>
+    </el-col>
+    <el-col class="searchbox" span="6">
+      <el-input
+        style="display: block"
+        placeholder="想要查找的userid"
+        clearable>
+      </el-input>
+    </el-col>
+    <el-col span="6">
+      <el-input
+        style="display: block"
+        placeholder="想要查找的aid"
+        clearable>
+      </el-input>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -39,3 +56,14 @@
     }
   }
 </script>
+
+<style scoped>
+.searchbox {
+  padding: 3px;
+  margin: auto;
+  background-color: #545C64;
+}
+.navbar {
+  background-color: #545C64;
+}
+</style>
